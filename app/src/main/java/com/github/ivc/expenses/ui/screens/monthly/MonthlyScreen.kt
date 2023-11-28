5ea7c447280d9c2a3a7603a4cbbbf57f12223636
ivc <ivc@users.noreply.github.com>
2023-11-28 20:19:18 +0400

    Add MonthTitle to MonthlyScreen

--- app/src/main/java/com/github/ivc/expenses/ui/screens/monthly/MonthlyScreen.kt
+++ app/src/main/java/com/github/ivc/expenses/ui/screens/monthly/MonthlyScreen.kt
@@ -2,30 +2,105 @@ package com.github.ivc.expenses.ui.screens.monthly
 
 import android.icu.util.Currency
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+private val headerFormat = DateTimeFormatter.ofPattern("yyyy\nMMMM")
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MonthlyScreen(currency: Currency, model: MonthlyViewModel = viewModel()) {
     val periods by (remember { model.months(currency) }).collectAsState()
+    val pagerState = rememberPagerState { periods.size }
 
     Column(Modifier.fillMaxSize()) {
         HorizontalPager(
-            state = rememberPagerState { periods.size },
-            modifier = Modifier.fillMaxSize(),
+            state = pagerState,
             reverseLayout = true,
         ) {
-            Text("${periods[it]}")
+            val period = periods[it]
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                MonthTitle(period, pagerState)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MonthTitle(timestamp: ZonedDateTime, pagerState: PagerState) {
+    val coroutineScope = rememberCoroutineScope()
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        IconButton(
+            onClick = {
+                coroutineScope.launch {
+                    pagerState.scrollToPage(pagerState.settledPage + 1, 0f)
+                }
+            },
+            enabled = pagerState.canScrollForward,
+        ) {
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowLeft,
+                contentDescription = "Previous Month",
+                modifier = Modifier.size(32.dp),
+            )
+        }
+
+        Text(
+            timestamp.format(headerFormat),
+            textAlign = TextAlign.Center,
+            minLines = 2,
+            maxLines = 2,
+            style = MaterialTheme.typography.headlineLarge,
+        )
+
+        IconButton(
+            onClick = {
+                coroutineScope.launch {
+                    pagerState.scrollToPage(pagerState.settledPage - 1, 0f)
+                }
+            },
+            enabled = pagerState.canScrollBackward,
+        ) {
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowRight,
+                contentDescription = "Next Month",
+                modifier = Modifier.size(32.dp),
+            )
         }
     }
 }
