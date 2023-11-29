a68143f67d28bbd959cdd52b33f7568dae0e6483
ivc <ivc@users.noreply.github.com>
2023-11-29 12:43:45 +0400

    Remove category icon size modifier

--- app/src/main/java/com/github/ivc/expenses/ui/screens/monthly/MonthlyScreen.kt
+++ app/src/main/java/com/github/ivc/expenses/ui/screens/monthly/MonthlyScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
@@ -131,7 +130,6 @@ fun CategoryListItem(category: Category, total: FormattedDouble, onClick: () -> 
                 painter = painterResource(id = category.icon.builtin.id),
                 contentDescription = category.name,
                 tint = Color(category.color),
-                modifier = Modifier.size(32.dp),
             )
         },
         trailingContent = {
